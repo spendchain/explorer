aad1ceffba01cfc0dec014bbdc3236c5d94382ec
kote-k <kotekumsishvili@gmail.com>
2020-11-12 19:39:45 -0700

    color and icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -126,7 +126,7 @@ module.exports = {
       "red-darkest": "#3b0d0c",
       "red-darker": "#621b18",
       "red-dark": "#cc1f1a",
-      red: "#EF192D", // UPDATED
+      red: "var(--color-theme-accents)", // UPDATED
       "red-light": "#ef5753",
       "red-lighter": "#f9acaa",
       "red-lightest": "#fcebea",
